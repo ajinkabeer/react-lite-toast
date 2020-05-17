affb27d8cce344f75af8a793f161a4147b9aa6dc
Ajin Kabeer <ajiinkabeer@gmail.com>
2020-05-17 23:48:17 +0530

    Add props in readme

--- README.md
+++ README.md
@@ -1,13 +1,20 @@
-# react-lite-toast
+<div align="center">
 
-> A minimal toast notification for react
+# React-lite-toast [![NPM](https://img.shields.io/npm/v/react-lite-toast.svg)](https://www.npmjs.com/package/react-lite-toast) [![JavaScript Style Guide](https://img.shields.io/badge/code_style-standard-brightgreen.svg)](https://standardjs.com)
 
-[![NPM](https://img.shields.io/npm/v/react-lite-toast.svg)](https://www.npmjs.com/package/react-lite-toast) [![JavaScript Style Guide](https://img.shields.io/badge/code_style-standard-brightgreen.svg)](https://standardjs.com)
+</div>
+
+A minimal toast notification for react with an unpacked size of 33.1 kB 🚀.
+
 
 ## Install
 
 ```bash
 npm install --save react-lite-toast
+```
+or
+```bash
+yarn add react-lite-toast
 ```
 
 ## Usage
@@ -29,6 +36,22 @@ const App = () => {
 
 export default App
 ```
+
+## Props
+
+| Prop                               | Description                                                        |
+| ---------------------------------- | ------------------------------------------------------------------ |
+| type `string`                      | Required. One of `success`, `error`, `warning`, `info`             |
+| title `string`                     | Required. The title of the toast notifaction.                      |
+| description `string `              | Required: The content of the toast notification                    |
+| position: `string    `             | Required. One of `bottomup`, `topdown`, `topleft`, `topright`,`bottomleft`, `bottomright`          |
+ 
+
+## Contributors
+
+- [Ajin Kabeer](https://github.com/ajinkabeer)
+
+
 
 ## License
 
--- dist/index.modern.js
+++ dist/index.modern.js
@@ -1,5 +1,23 @@
 import React, { useState, useEffect } from 'react';
 
+function _extends() {
+  _extends = Object.assign || function (target) {
+    for (var i = 1; i < arguments.length; i++) {
+      var source = arguments[i];
+
+      for (var key in source) {
+        if (Object.prototype.hasOwnProperty.call(source, key)) {
+          target[key] = source[key];
+        }
+      }
+    }
+
+    return target;
+  };
+
+  return _extends.apply(this, arguments);
+}
+
 var done = require("./check~YoXmxPYW.svg");
 
 var error = require("./error~sZYKhomM.svg");
@@ -10,98 +28,107 @@ var warning = require("./warning~vKUgrCLp.svg");
 
 var close = require("./close~MmoXfehv.svg");
 
-var Style = {"toastcontainer":"_styles-module__toastcontainer__15L_e","toast":"_styles-module__toast__3KnTL","toasttitle":"_styles-module__toasttitle__2xY3H","toastmessage":"_styles-module__toastmessage__2aPZH","toastimage":"_styles-module__toastimage__MQjN0","snack":"_styles-module__snack__298KZ","closebutton":"_styles-module__closebutton__1bDA9","topright":"_styles-module__topright__18X5b","animatefromright":"_styles-module__animatefromright__2Fqj2","bottomright":"_styles-module__bottomright__S5YOC","topleft":"_styles-module__topleft__3xLQ5","animatefromleft":"_styles-module__animatefromleft__2Gun4","bottomleft":"_styles-module__bottomleft__rqvi6","bottomup":"_styles-module__bottomup__2V_fh","animatefrombottom":"_styles-module__animatefrombottom__qwi-u","topdown":"_styles-module__topdown__1O4DU","animatefromtop":"_styles-module__animatefromtop__Smj_t"};
+var Style = {"toastcontainer":"_15L_e","toast":"_3KnTL","toasttitle":"_2xY3H","toastmessage":"_2aPZH","toastimage":"_MQjN0","snack":"_298KZ","closebutton":"_1bDA9","topright":"_18X5b","animatefromright":"_2Fqj2","bottomright":"_S5YOC","topleft":"_3xLQ5","animatefromleft":"_2Gun4","bottomleft":"_rqvi6","bottomup":"_2V_fh","animatefrombottom":"_qwi-u","topdown":"_1O4DU","animatefromtop":"_Smj_t"};
 
-const Toast = ({
-  type,
-  title,
-  description,
-  position
-}) => {
-  const [toast, setToast] = useState([]);
-  useEffect(() => {
+var Toast = function Toast(_ref) {
+  var type = _ref.type,
+      title = _ref.title,
+      description = _ref.description,
+      position = _ref.position;
+
+  var _useState = useState([]),
+      toast = _useState[0],
+      setToast = _useState[1];
+
+  useEffect(function () {
     setToastType(type);
   }, [type]);
-  useEffect(() => {
-    const interval = setInterval(() => {
+  useEffect(function () {
+    var interval = setInterval(function () {
       if (toast.length) {
         closeToast(toast[0].title);
       }
     }, 2000);
-    return () => {
+    return function () {
       clearInterval(interval);
     };
   }, [toast]);
 
-  const setToastType = type => {
+  var setToastType = function setToastType(type) {
     switch (type) {
       case 'success':
-        setToast([...toast, {
-          title,
-          description,
+        setToast([].concat(toast, [{
+          title: title,
+          description: description,
           backgroundColor: '#2ea05d',
           icon: done
-        }]);
+        }]));
         break;
 
       case 'error':
-        setToast([...toast, {
-          title,
-          description,
+        setToast([].concat(toast, [{
+          title: title,
+          description: description,
           backgroundColor: '#E53E3E',
           icon: error
-        }]);
+        }]));
         break;
 
       case 'info':
-        setToast([...toast, {
-          title,
-          description,
+        setToast([].concat(toast, [{
+          title: title,
+          description: description,
           backgroundColor: '#3182ce',
           icon: info
-        }]);
+        }]));
         break;
 
       case 'warning':
-        setToast([{ ...toast,
-          title,
-          description,
+        setToast([_extends(_extends({}, toast), {}, {
+          title: title,
+          description: description,
           backgroundColor: '#DD6B20',
           icon: warning
-        }]);
+        })]);
         break;
     }
   };
 
-  const closeToast = title => {
-    const index = toast.findIndex(e => e.title === title);
+  var closeToast = function closeToast(title) {
+    var index = toast.findIndex(function (e) {
+      return e.title === title;
+    });
     toast.splice(index, 1);
-    setToast([...toast]);
+    setToast([].concat(toast));
   };
 
   return React.createElement("div", {
-    className: `${Style.toastcontainer}  ${Style[position]}`
-  }, toast.map((toast, i) => React.createElement("div", {
-    key: i,
-    className: `${Style.toast} ${Style.snack}  ${Style[position]}`,
-    style: {
-      backgroundColor: toast.backgroundColor
-    }
-  }, React.createElement("img", {
-    className: `${Style.closebutton}`,
-    src: close,
-    alt: "close-button",
-    onClick: () => closeToast(toast.title)
-  }), React.createElement("div", {
-    className: `${Style.toastimage}`
-  }, React.createElement("img", {
-    src: toast.icon,
-    alt: ""
-  })), React.createElement("div", null, React.createElement("p", {
-    className: `${Style.toasttitle}`
-  }, toast.title), React.createElement("p", {
-    className: `${Style.toastmessage}`
-  }, toast.description)))));
+    className: Style.toastcontainer + "  " + Style[position]
+  }, toast.map(function (toast, i) {
+    return React.createElement("div", {
+      key: i,
+      className: Style.toast + " " + Style.snack + "  " + Style[position],
+      style: {
+        backgroundColor: toast.backgroundColor
+      }
+    }, React.createElement("img", {
+      className: "" + Style.closebutton,
+      src: close,
+      alt: "close-button",
+      onClick: function onClick() {
+        return closeToast(toast.title);
+      }
+    }), React.createElement("div", {
+      className: "" + Style.toastimage
+    }, React.createElement("img", {
+      src: toast.icon,
+      alt: ""
+    })), React.createElement("div", null, React.createElement("p", {
+      className: "" + Style.toasttitle
+    }, toast.title), React.createElement("p", {
+      className: "" + Style.toastmessage
+    }, toast.description)));
+  }));
 };
 
 export { Toast };
